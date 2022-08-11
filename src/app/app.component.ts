dde04fcd4a9c6dee8f05ff6f2b186c8e57d4c16a
Paula PB <pa02perez21d@gmail.com>
2022-08-10 23:46:48 -0500

    tarea1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  resultado: number;
+  resultado1: number;
+  resultado2: number;
+  constructor(){
+
+  }
+  ngOnInit() { 
+    //this.suma() 
+  }
+  suma(){
+    this.resultado = this.resultado1 + this.resultado2;
+  }
+  resta(){
+    this.resultado = this.resultado1 - this.resultado2;
+  }
+  multiplicacion(){
+    this.resultado = this.resultado1 * this.resultado2;
+  }
+  /*imprimir(){
+    console.log(this.prueba1);
+    
+  }*/
 }
